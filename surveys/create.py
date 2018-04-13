fb3eb78855f0cd1c8d63f74f5ba2e13ad5464a09
Taechit <taechit.c@gmail.com>
2018-04-13 21:01:30 +0700

    Add try and except

--- surveys/create.py
+++ surveys/create.py
@@ -17,46 +17,57 @@ def create(event, context):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-    stStatus =  data['status']
-    item = {
-            'id': str(uuid.uuid1()),
-            'fname': data['fname'],
-            'lname': data['lname'],
-            'nickname': data['nickname'],
-            'gender': data['gender'],
-            'room': data['room'],
-            'no': data['no'],
-            'status': stStatus,
-            'createdAt': timestamp,
-            'updatedAt': timestamp,
+    try :
+        stStatus =  data['status']
+        item = {
+                'id': str(uuid.uuid1()),
+                'fname': data['fname'],
+                'lname': data['lname'],
+                'nickname': data['nickname'],
+                'gender': data['gender'],
+                'room': data['room'],
+                'no': data['no'],
+                'status': stStatus,
+                'createdAt': timestamp,
+                'updatedAt': timestamp,
+            }
+
+        if stStatus == '1':
+            item.update({
+                'university':data['university'],
+                'faculty':data['faculty'],
+                'major':data['major'],
+                'round':data['round'],
+                'confirm':data['confirm']
+            })
+        elif stStatus =='2' :
+            item.update({
+                'university':data['university'],
+                'faculty':data['faculty'],
+                'major':data['major']
+            })
+        elif stStatus == '5':
+            item.update({
+                'condition':data['condition']
+            })
+            
+        # write the todo to the database
+        table.put_item(Item=item)
+
+        # create a response
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(item)
         }
-
-    if stStatus == '1':
-        item.update({
-            'university':data['university'],
-            'faculty':data['faculty'],
-            'major':data['major'],
-            'round':data['round'],
-            'confirm':data['confirm']
-        })
-    elif stStatus =='2' :
-        item.update({
-            'university':data['university'],
-            'faculty':data['faculty'],
-            'major':data['major']
-        })
-    elif stStatus == '5':
-        item.update({
-            'condition':data['condition']
-        })
-        
-    # write the todo to the database
-    table.put_item(Item=item)
-
-    # create a response
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(item)
-    }
+    except ValueError:
+        response = {
+            "statusCode": 504,
+            "body": json.dumps(item)
+        }
+    except:
+        response = {
+            "statusCode": 500,
+            "body": json.dumps(item)
+        }
 
     return response
